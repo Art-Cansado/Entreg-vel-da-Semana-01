d526c55a50a58d1a45debb8d4ece3286378c6970
Arthur Gabriel <arthursarges5@gmail.com>
2026-09-21 09:55:27 -0300

    Update desafio_05_par_impar.py

--- desafio_05_par_impar.py
+++ desafio_05_par_impar.py
@@ -1,6 +1,12 @@
+# Solicita um número inteiro ao usuário
 numero = int(input("Digite um número inteiro: "))
 
+# O operador % calcula o resto da divisão
+# Se o resto da divisão por 2 for igual a 0, o número é par
 if numero % 2 == 0:
+    # Executado quando o número é divisível por 2
     print("O número é par.")
+
 else:
+    # Executado quando o número não é divisível por 2
     print("O número é ímpar.")
