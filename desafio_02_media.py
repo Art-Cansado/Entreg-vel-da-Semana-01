f05c7e551420a9b95c4e9b9a1d08051bce618235
Arthur Gabriel <arthursarges5@gmail.com>
2026-09-21 09:53:58 -0300

    Update desafio_02_media.py

--- desafio_02_media.py
+++ desafio_02_media.py
@@ -1,7 +1,14 @@
+# Solicita a primeira nota e converte o valor para número decimal
 nota_1 = float(input("Digite a primeira nota: "))
+
+# Solicita a segunda nota e converte o valor para número decimal
 nota_2 = float(input("Digite a segunda nota: "))
+
+# Solicita a terceira nota e converte o valor para número decimal
 nota_3 = float(input("Digite a terceira nota: "))
 
+# Soma as três notas e divide por 3 para calcular a média
 media = (nota_1 + nota_2 + nota_3) / 3
 
+# Exibe a média com duas casas decimais
 print(f"Média: {media:.2f}")
