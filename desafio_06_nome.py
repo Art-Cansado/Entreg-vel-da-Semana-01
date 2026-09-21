045d89836fe285d1ced43abd8a939c384fa9896d
Arthur Gabriel <arthursarges5@gmail.com>
2026-09-21 09:55:46 -0300

    Invert user name and display it

--- desafio_06_nome.py
+++ desafio_06_nome.py
@@ -1,5 +1,9 @@
+# Solicita o nome do usuário
 nome = input("Digite seu nome: ")
 
+# [::-1] percorre a string de trás para frente,
+# invertendo a ordem dos caracteres
 nome_invertido = nome[::-1]
 
+# Exibe o nome na ordem invertida
 print(f"Nome invertido: {nome_invertido}")
